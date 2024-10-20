d4459d4c2e40420b26535ed8796279cb04825e7f
Calebe-Dve <calebe.araujo101@gmail.com>
2024-10-20 15:40:02 -0300

    Projeto Recriado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { KnowledgeComponent } from './knowledge/knowledge.component';
+import { WorksComponent } from './works/works.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'/home', pathMatch:'full'},
+  {path: 'home', component:HomeComponent},
+  {path:'skills', component:KnowledgeComponent},
+  {path: 'works', component:WorksComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BioComponent } from './bio/bio.component';
+import { HeroComponent } from './hero/hero.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { WorksComponent } from './works/works.component';
+import { KnowledgeComponent } from './knowledge/knowledge.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BioComponent,
+    HeroComponent,
+    SkillsComponent,
+    ContactComponent,
+    ServicesComponent,
+    NavBarComponent,
+    HomeComponent,
+    WorksComponent,
+    KnowledgeComponent
   ],
   imports: [
     BrowserModule,
